7f6c5f5ddc2f10c69e15516c55b4b3ed5d8f9316
Jeffrey191997 <kgmyatun97@gmail.com>
2023-05-15 17:00:17 +0700

    Third Commit

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -13,7 +13,7 @@ const Home = ({ user = {}, setStatus = () => {} }) => {
   const [openCreate, setOpenCreate] = useState(false);
   const [notes, setNotes] = useState([]);
   const navigate = useNavigate();
-  const { setUser } = useContext(GlobalContext);
+  const {user, setUser } = useContext(GlobalContext);
 
   useEffect(() => {
     const userToken = Cookies.get('UserToken');
